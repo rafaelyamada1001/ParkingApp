434cd893296ab64365b10b9072a92ab85a431477
Rafael Yamada <rafaelyamada2001@gmail.com>
2024-10-21 15:24:45 -0300

    CriandoRegrasDeNegocio

--- ParkingApp/Management/Estacionamento.cs
+++ ParkingApp/Management/Estacionamento.cs
@@ -2,14 +2,43 @@
 {
     public class Estacionamento
     {
-        private List<Veiculo> veiculosEstacionados = new List<Veiculo>();
-        private int totalVagas;
+        private List<Veiculo> veiculos = new List<Veiculo>();
         private decimal valorHora;
+        public int vagas;
 
         public Estacionamento(int vagas, decimal valor)
         {
-            vagas = totalVagas;
             valor = valorHora;
+            this.vagas = vagas;
+        }
+
+
+
+        public void AdicionarVeiculo(string placa)
+        {
+            var veiculo = new Veiculo(placa);
+            veiculos.Add(veiculo);
+            Console.WriteLine($"Veículo {placa} adicionado ao estacionamento.");
+        }
+
+        public void RemoverVeiculo(string placa)
+        {
+
+
+        }
+        public void ListarVeiculos()
+        {
+            if (veiculos.Count == 0)
+            {
+                Console.WriteLine("Nenhum veículo estacionado.");
+                return;
+            }
+
+            Console.WriteLine($"Veículos estacionados:{veiculos.Count}");
+            foreach (var v in veiculos)
+            {
+                Console.WriteLine($"- {v.PlacaVeiculo}, Entrada: {v.HoraEntrada}");
+            }
         }
     }
 }
---
+++ ParkingApp/Management/GerenciadorOpcoes.cs
@@ -0,0 +1,48 @@
+﻿namespace ParkingApp.Management
+{
+    public static class GerenciadorOpcoes
+    {
+        public static void GerEstacionamento()
+        {
+            var estacionamento = new Estacionamento(10, 5.00m);
+            while (true)
+            {
+                Console.WriteLine("\nSistema de Estacionamento");
+                Console.WriteLine("0 - Sair");
+                Console.WriteLine("1 - Estacionar carro");
+                Console.WriteLine("2 - Retirar carro");
+                Console.WriteLine("3 - Listar carros estacionados");
+                Console.WriteLine("4 - Exibir vagas disponíveis");
+
+                Console.Write("Escolha uma opção: ");
+                string registerPark = Console.ReadLine();
+
+                switch (registerPark)
+                {
+                    case "0":
+                        return;
+                    case "1":
+                        Console.WriteLine("Digite a Placa do Carro");
+                        var placa = Console.ReadLine();
+                        if(placa == null)
+                        {
+                            Console.WriteLine("Valor não pode ser nulo");
+                        }                      
+                        estacionamento.AdicionarVeiculo(placa);
+                        break;
+                    case "2":
+                        Console.Write("Digite a placa do veículo: ");
+                        placa = Console.ReadLine();
+                        estacionamento.RemoverVeiculo(placa);
+                        break;
+                    case "3":
+                        estacionamento.ListarVeiculos();
+                        break;
+                    case "4":
+                        Console.WriteLine($"Vagas Disponíveis:");
+                        break;
+                }
+            }
+        }
+    }
+}
